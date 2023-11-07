fefba6edc0892eb4908e89c8b54473d058e65bf5
Shireee <kirog11az@gmail.com>
2023-11-07 22:50:37 +0300

    Complete 1.1.7

---
+++ Assets/Scripts/HeaderScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class HeaderScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnClickHandler(int index)
+    {
+        if (index >= 0)
+            SceneManager.LoadScene(index);
+        else if (index == -1)
+        {
+            Application.Quit();
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        }
+    }
+}
