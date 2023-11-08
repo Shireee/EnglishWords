c2d11e725e6239cb4cebc25f430138fd6defb49c
Shireee <kirog11az@gmail.com>
2023-11-08 23:44:37 +0300

    Complete 1.1.10

--- Assets/Scripts/HeaderScript.cs
+++ Assets/Scripts/HeaderScript.cs
@@ -2,25 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class HeaderScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Button lBut, rBut;
+    int curInd;
     void Start()
     {
-
+        lBut = transform.GetChild(0).GetComponent<Button>();
+        rBut = transform.GetChild(1).GetComponent<Button>(); // getting first child !guide error
+        curInd = SceneManager.GetActiveScene().buildIndex;
     }
-
-    // Update is called once per frame
     void Update()
     {
-
+        if (!Input.anyKeyDown)
+            return;
+        if (Input.GetKeyDown(KeyCode.Escape))
+            OnClickHandler(-1);
+        else if (Input.GetKeyDown(KeyCode.F5)) // move to learing
+            OnClickHandler(1);
+        else if (Input.GetKeyDown(KeyCode.F6)) // move to contol
+            OnClickHandler(2);
+        else if (Input.GetKeyDown(KeyCode.F7)) // move to result
+            OnClickHandler(3);
+        else if (Input.GetKeyDown(KeyCode.F2)) // move to settings
+            OnClickHandler(4);
+        else if (Input.GetKeyDown(KeyCode.F1)) // move to help
+            OnClickHandler(5);
+        else if (Input.GetKeyDown(KeyCode.F3)
+        && lBut.IsActive() && lBut.interactable) // header left button
+            lBut.onClick.Invoke();
+        else if (Input.GetKeyDown(KeyCode.F4) // header right button
+        && rBut.IsActive() && rBut.interactable)
+            rBut.onClick.Invoke();
     }
     public void OnClickHandler(int index)
     {
         if (index >= 0)
-            SceneManager.LoadScene(index);
+        {
+            if (index != curInd)
+                SceneManager.LoadScene(index);
+        }
         else if (index == -1)
         {
             Application.Quit();
